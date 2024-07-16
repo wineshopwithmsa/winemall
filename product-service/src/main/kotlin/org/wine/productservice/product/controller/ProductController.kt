21705be03a4a4f81f5cea5cfb4bf58404463a92c
choiseongjun <csj2702@naver.com>
2024-07-16 21:01:34 +0900

    [최성준] 1.common lib 수정 2.jwt token decode commonlib에서 불러옴

--- product-service/src/main/kotlin/org/wine/productservice/product/controller/ProductController.kt
+++ product-service/src/main/kotlin/org/wine/productservice/product/controller/ProductController.kt
@@ -1,21 +1,21 @@
 package org.wine.productservice.product.controller
 
-import com.netflix.discovery.converters.Auto
-import org.springframework.beans.factory.annotation.Autowired
+import jwt.JwtTokenProvider
 import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.wine.productservice.product.service.JwtTokenProvider
+
 
 @RestController
 @RequestMapping("/api/product")
 class ProductController {
-    @Autowired lateinit var jwtTokenProvider: JwtTokenProvider
+
 
     @GetMapping("/v1")
     fun getTest(@RequestHeader headers: HttpHeaders): String {
+        val jwtTokenProvider = JwtTokenProvider()
         val authorizationHeader = headers.getFirst(HttpHeaders.AUTHORIZATION)
 
         return if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
